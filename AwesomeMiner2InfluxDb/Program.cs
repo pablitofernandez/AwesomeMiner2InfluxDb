d033154e6dd6a735cd5cc69577760642f3732de0
pablo <pablo.fernandez@deusto.es>
2018-02-14 12:51:12 +0100

    Add project files.

---
+++ AwesomeMiner2InfluxDb/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeMiner2InfluxDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
