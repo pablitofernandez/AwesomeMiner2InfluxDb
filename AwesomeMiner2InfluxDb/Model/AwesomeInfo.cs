8277771e0dff893fcc1492c636794d102715776f
pablo <pablo.fernandez@deusto.es>
2018-02-23 18:03:40 +0100

    First Commit

---
+++ AwesomeMiner2InfluxDb/Model/AwesomeInfo.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeMiner2InfluxDb.Model
+{
+    public class AwesomeInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string hostname { get; set; }
+        public int groupId { get; set; }
+        public string pool { get; set; }
+        public string temperature { get; set; }
+        public object hardware { get; set; }
+        public Statusinfo statusInfo { get; set; }
+        public Progressinfo progressInfo { get; set; }
+        public Speedinfo speedInfo { get; set; }
+        public Coininfo coinInfo { get; set; }
+        public DateTime updatedUtc { get; set; }
+        public string updated { get; set; }
+        public Poollist[] poolList { get; set; }
+        public Gpulist[] gpuList { get; set; }
+        public object[] pgaList { get; set; }
+        public object[] asicList { get; set; }
+        public bool hasPool { get; set; }
+        public bool hasGpu { get; set; }
+        public bool hasPga { get; set; }
+        public bool hasAsic { get; set; }
+        public bool canReboot { get; set; }
+        public bool canStop { get; set; }
+        public bool canRestart { get; set; }
+        public bool canStart { get; set; }
+        public bool canPool { get; set; }
+        public bool hasValidStatus { get; set; }
+        public Metadata metaData { get; set; }
+    }
+
+    public class Statusinfo
+    {
+        public string statusDisplay { get; set; }
+        public string statusLine3 { get; set; }
+    }
+
+    public class Progressinfo
+    {
+        public string line1 { get; set; }
+        public string line2 { get; set; }
+        public string line3 { get; set; }
+    }
+
+    public class Speedinfo
+    {
+        public int logInterval { get; set; }
+        public string hashrate { get; set; }
+        public int hashrateValue { get; set; }
+        public string avgHashrate { get; set; }
+        public string workUtility { get; set; }
+        public object line2 { get; set; }
+        public object line3 { get; set; }
+    }
+
+    public class Coininfo
+    {
+        public string displayName { get; set; }
+        public string revenuePerDay { get; set; }
+        public float revenuePerDayValue { get; set; }
+        public string revenuePerMonth { get; set; }
+        public float profitPerDayValue { get; set; }
+        public string profitPerDay { get; set; }
+        public string profitPerMonth { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string updated { get; set; }
+        public string edition { get; set; }
+        public string version { get; set; }
+        public object[] infoList { get; set; }
+        public object[] warningList { get; set; }
+    }
+
+    public class Poollist
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public PoolStatusinfo statusInfo { get; set; }
+        public Additionalinfo additionalInfo { get; set; }
+        public Priorityinfo priorityInfo { get; set; }
+        public PoolProgressinfo progressInfo { get; set; }
+        public string coinName { get; set; }
+        public int minerID { get; set; }
+        public string minerName { get; set; }
+        public bool canRemove { get; set; }
+        public bool canDisable { get; set; }
+        public bool canEnable { get; set; }
+        public bool canPrioritize { get; set; }
+    }
+
+    public class PoolStatusinfo
+    {
+        public string statusDisplay { get; set; }
+        public object statusLine3 { get; set; }
+    }
+
+    public class Additionalinfo
+    {
+        public string displayUrl { get; set; }
+        public string worker { get; set; }
+    }
+
+    public class Priorityinfo
+    {
+        public int priority { get; set; }
+        public int quota { get; set; }
+    }
+
+    public class PoolProgressinfo
+    {
+        public string line1 { get; set; }
+        public string line2 { get; set; }
+        public string line3 { get; set; }
+    }
+
+    public class Gpulist
+    {
+        public string name { get; set; }
+        public GpuStatusinfo statusInfo { get; set; }
+        public Deviceinfo deviceInfo { get; set; }
+        public GpuProgressinfo progressInfo { get; set; }
+        public GpuSpeedinfo speedInfo { get; set; }
+    }
+
+    public class GpuStatusinfo
+    {
+        public string statusDisplay { get; set; }
+        public string statusLine3 { get; set; }
+    }
+
+    public class Deviceinfo
+    {
+        public string deviceType { get; set; }
+        public int gpuActivity { get; set; }
+        public string intensity { get; set; }
+        public object name { get; set; }
+        public int gpuClock { get; set; }
+        public int gpuMemoryClock { get; set; }
+        public string gpuVoltage { get; set; }
+        public int gpuPowertune { get; set; }
+        public int fanSpeed { get; set; }
+        public int fanPercent { get; set; }
+        public int temperature { get; set; }
+    }
+
+    public class GpuProgressinfo
+    {
+        public string line1 { get; set; }
+        public string line2 { get; set; }
+        public string line3 { get; set; }
+    }
+
+    public class GpuSpeedinfo
+    {
+        public int logInterval { get; set; }
+        public string hashrate { get; set; }
+        public int hashrateValue { get; set; }
+        public string avgHashrate { get; set; }
+        public string workUtility { get; set; }
+        public object line2 { get; set; }
+        public object line3 { get; set; }
+    }
+
+}
--- AwesomeMiner2InfluxDb/Program.cs
+++ AwesomeMiner2InfluxDb/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using AdysTech.InfluxDB.Client.Net;
+using AwesomeMiner2InfluxDb.Model;
 
 namespace AwesomeMiner2InfluxDb
 {
@@ -10,6 +17,94 @@ namespace AwesomeMiner2InfluxDb
     {
         static void Main(string[] args)
         {
+            string sSource;
+            string sLog;
+            string sEvent;
+
+            sSource = "AwesomeMiner2InfluxDb";
+            sLog = "Application";
+
+            if (!EventLog.SourceExists(sSource))
+                EventLog.CreateEventSource(sSource, sLog);
+
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                var host = appSettings["Host"];
+                var minerId = appSettings["MinerId"];
+                var url = $"http://{host}/api/miners/{minerId}";
+                var request = WebRequest.Create(url);
+                request.ContentType = "application/json; charset=utf-8";
+                string text;
+                var response = (HttpWebResponse)request.GetResponse();
+                Console.WriteLine("Reading json data from AwesomeMiner api");
+                using (var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    text = sr.ReadToEnd();
+                }
+
+                AwesomeInfo awesomeInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<AwesomeInfo>(text);
+                Console.WriteLine("Json received and parsed");
+                var influxUrl = appSettings["InfluxDbHost"];
+                var influxDbName = appSettings["InfluxDbName"];
+
+                Console.WriteLine($"Connecting to Influxdb @ {influxUrl} DBName: {influxDbName}");
+                InfluxDBClient client = new InfluxDBClient($"http://{influxUrl}");
+                client.CreateDatabaseAsync(influxDbName).Wait();
+                var utcNow = DateTime.UtcNow;
+                var valMixed = new InfluxDatapoint<InfluxValueField>();
+                valMixed.UtcTimestamp = utcNow;
+                valMixed.Tags.Add("MinerId", awesomeInfo.id.ToString());
+                valMixed.Tags.Add("Algo", awesomeInfo.coinInfo.displayName);
+                valMixed.Tags.Add("Pool", awesomeInfo.poolList[0].name);
+                var hashRate = awesomeInfo.speedInfo.hashrate.Substring(0, awesomeInfo.speedInfo.hashrate.IndexOf(" "));
+                var hashRateDouble = double.Parse(hashRate);
+                valMixed.Fields.Add("HashRate", new InfluxValueField(hashRateDouble));
+                var profitPerday = double.Parse(awesomeInfo.coinInfo.profitPerDay.Substring(1));
+                var profitPerMonth = double.Parse(awesomeInfo.coinInfo.profitPerMonth.Substring(1));
+                var revenuePerDay = double.Parse(awesomeInfo.coinInfo.revenuePerDay.Substring(1));
+                var revenuePerMonth = double.Parse(awesomeInfo.coinInfo.revenuePerMonth.Substring(1));
+                valMixed.Fields.Add("ProfitPerDay", new InfluxValueField(profitPerday));
+                valMixed.Fields.Add("ProfitPerMonth", new InfluxValueField(profitPerMonth));
+                valMixed.Fields.Add("RevenuePerDay", new InfluxValueField(revenuePerDay));
+                valMixed.Fields.Add("RevenuePerMonth", new InfluxValueField(revenuePerMonth));
+
+
+                valMixed.MeasurementName = $"Miner.{awesomeInfo.id}";
+
+                Console.WriteLine("Trying to write miner data to DB");
+                client.PostPointAsync(influxDbName, valMixed).Wait();
+                EventLog.WriteEntry(sSource, "Miner data written successfully", EventLogEntryType.Information);
+                Console.WriteLine("Miner data written successfully");
+
+                foreach (var gpuInfo in awesomeInfo.gpuList)
+                {
+                    var gpuToInflux = new InfluxDatapoint<InfluxValueField>();
+                    gpuToInflux.UtcTimestamp = utcNow;
+                    gpuToInflux.Tags.Add("GPUName", gpuInfo.name);
+                    gpuToInflux.Fields.Add("Temperature", new InfluxValueField(gpuInfo.deviceInfo.temperature));
+                    gpuToInflux.Fields.Add("FanPercent", new InfluxValueField(gpuInfo.deviceInfo.fanPercent));
+                    gpuToInflux.Fields.Add("FanSpeed", new InfluxValueField(gpuInfo.deviceInfo.fanSpeed));
+                    gpuToInflux.Fields.Add("GpuClock", new InfluxValueField(gpuInfo.deviceInfo.gpuClock));
+                    gpuToInflux.Fields.Add("MemoryClock", new InfluxValueField(gpuInfo.deviceInfo.gpuMemoryClock));
+                    var intensity = double.Parse(gpuInfo.deviceInfo.intensity);
+                    gpuToInflux.Fields.Add("Intensity", new InfluxValueField(intensity));
+                    var gpuHashrate = gpuInfo.speedInfo.hashrate.Substring(0, gpuInfo.speedInfo.hashrate.IndexOf(" "));
+                    var gpuHashrateDouble = double.Parse(gpuHashrate);
+                    gpuToInflux.Fields.Add("Speed", new InfluxValueField(gpuHashrateDouble));
+
+                    gpuToInflux.MeasurementName = gpuInfo.name.Replace(' ', '_');
+
+                    Console.WriteLine("Trying to write GPU info to DB");
+                    client.PostPointAsync(influxDbName, gpuToInflux).Wait();
+                    Console.WriteLine("GPU info written successfully");
+                }
+            }
+            catch (Exception e)
+            {
+                // Console.WriteLine(e);
+                EventLog.WriteEntry(sSource, e.Message, EventLogEntryType.Error);
+            }
         }
     }
 }
